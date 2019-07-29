7b736d6c9e4105d26fafc0de010baf1841ff5216
Joe Gannon <joegannon15@gmail.com>
2019-07-29 12:27:30 -0400

    hooking functions

--- TrackSales.lua
+++ TrackSales.lua
@@ -1,9 +1,35 @@
 
-TrackSales = LibStub("AceAddon-3.0"):NewAddon("TrackSales", "AceConsole-3.0")
+TrackSales = LibStub("AceAddon-3.0"):NewAddon("TrackSales", "AceConsole-3.0", "AceHook-3.0")
 
-function TrackSales:OnInitialize()
+function TrackSales:OnInitialize()	
 
-	--message('HelloWorld')
-	self:Print("Hello World!")	
+	self:Hook("TakeInboxMoney", true)
+	self:Hook("AutoLootMailItem", true)
 
 end
+
+function TrackSales:TakeInboxMoney(...)
+	
+	self:Print("Take Money")
+
+	local mailIndex  = ...
+
+	local invoiceType, itemName, playerName, bid, buyout, deposit, consignment = GetInboxInvoiceInfo(mailIndex);
+
+	if invoiceType and invoiceType == "seller" then
+		self:Print("invoiceType, itemName, playerName, bid, buyout, deposit, consignment - ", invoiceType, itemName, playerName, bid, buyout, deposit, consignment)
+	end
+end 
+
+
+function TrackSales:AutoLootMailItem(...)
+
+	self:Print("Auto Loot Mail Item")
+	self:TakeInboxMoney(...)
+end 
+
+function TrackSales:test(...)
+
+	self:Print("test")	
+	
+end 
