e12af82e11681ad3eec55bf7fe12684bd17cc3b2
Joe Gannon <joegannon15@gmail.com>
2019-07-29 13:25:17 -0400

    profession debugging

--- TrackSales.lua
+++ TrackSales.lua
@@ -4,7 +4,7 @@ TrackSales = LibStub("AceAddon-3.0"):NewAddon("TrackSales", "AceConsole-3.0", "A
 function TrackSales:OnInitialize()	
 
 	self:Hook("TakeInboxMoney", true)
-	self:Hook("AutoLootMailItem", true)
+	self:Hook("AutoLootMailItem", true)	
 
 end
 
@@ -24,19 +24,41 @@ function TrackSales:TakeInboxMoney(...)
 
 		self:Print(sender, subject, money)
 	end
-
-
 end 
 
 
 function TrackSales:AutoLootMailItem(...)
-
 	self:Print("Auto Loot Mail Item")
 	self:TakeInboxMoney(...)
 end 
 
-function TrackSales:test(...)
+function TrackSales:Debug(...)
 
-	self:Print("test")	
-	
+  local prof1, prof2, sec1, sec2, sec3, sec4 = self:GetProfessionDetails()
+
+  self:Print(prof1, prof2, sec1, sec2, sec3, sec4)
 end 
+
+function TrackSales:GetProfessionDetails()
+
+	local prof1, prof2, sec1, sec2, sec3, sec4 = GetProfessions()	
+	
+	local prof1Name = self:GetProfessionName(prof1)
+	local prof2Name = self:GetProfessionName(prof2)
+	local sec1Name = self:GetProfessionName(sec1)
+	local sec2Name = self:GetProfessionName(sec2)
+	local sec3Name = self:GetProfessionName(sec3)
+	local sec4Name = self:GetProfessionName(sec4)
+
+	return prof1Name, prof2Name, sec1Name, sec2Name, sec3Name, sec4Name
+end
+
+function TrackSales:GetProfessionName(index)
+	if (index) then
+		local name = GetProfessionInfo(index); 
+		
+		return name
+	else 
+		return nil
+	end
+end
