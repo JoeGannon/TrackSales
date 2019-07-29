c4c26423dc83e0caa09c66c846652dd30a2f2ef4
Joe Gannon <joegannon15@gmail.com>
2019-07-29 14:44:01 -0400

    beginning db

--- TrackSales.lua
+++ TrackSales.lua
@@ -4,7 +4,15 @@ TrackSales = LibStub("AceAddon-3.0"):NewAddon("TrackSales", "AceConsole-3.0", "A
 function TrackSales:OnInitialize()	
 
 	self:Hook("TakeInboxMoney", true)
-	self:Hook("AutoLootMailItem", true)	
+	self:Hook("AutoLootMailItem", true)
+
+
+	if not TrackSalesDB then
+		self:Print("is null")
+
+	else 
+		self:Print("is not null")
+	end 
 
 end
 
@@ -14,13 +22,13 @@ function TrackSales:TakeInboxMoney(...)
 
 	local mailIndex  = ...
 
-	local invoiceType, itemName, playerName, bid, buyout, deposit, consignment = GetInboxInvoiceInfo(mailIndex);
+	local invoiceType, itemName, playerName, bid, buyout, deposit, consignment = GetInboxInvoiceInfo(mailIndex)
 
 	if invoiceType and invoiceType == "seller" then
 		self:Print("invoiceType, itemName, playerName, bid, buyout, deposit, consignment - ", invoiceType, itemName, playerName, bid, buyout, deposit, consignment)
 	else 
 
-		local _, _, sender, subject, money = GetInboxHeaderInfo(mailIndex);
+		local _, _, sender, subject, money = GetInboxHeaderInfo(mailIndex)
 
 		self:Print(sender, subject, money)
 	end
@@ -32,14 +40,22 @@ function TrackSales:AutoLootMailItem(...)
 	self:TakeInboxMoney(...)
 end 
 
-function TrackSales:Debug(...)
+function TrackSales:Debug(var)
 
-  local prof1, prof2, sec1, sec2, sec3, sec4 = self:GetProfessionDetails()
+	TrackSalesDB = var
 
+  local prof1, prof2, sec1, sec2, sec3, sec4 = self:LookupProfessions()
+  
   self:Print(prof1, prof2, sec1, sec2, sec3, sec4)
 end 
 
-function TrackSales:GetProfessionDetails()
+function TrackSales:CV(...)
+
+	TrackSalesDB = nil
+
+end 
+
+function TrackSales:LookupProfessions()
 
 	local prof1, prof2, sec1, sec2, sec3, sec4 = GetProfessions()	
 	
@@ -55,10 +71,52 @@ end
 
 function TrackSales:GetProfessionName(index)
 	if (index) then
-		local name = GetProfessionInfo(index); 
+		local name = GetProfessionInfo(index)
 		
 		return name
 	else 
 		return nil
 	end
+end
+
+function TrackSales:SetDefaults()
+	local prof1, prof2, sec1, sec2, sec3, sec4 = self:LookupProfessions()
+
+	local people = {
+		Professions = {	}	 
+	 }
+
+	 people.Professions = {
+		{
+			name = "Fred",
+			address = "16 Long Street",
+			phone = "123456"
+	   }
+	  
+		--  {
+		-- 	name = "Wilma",
+		-- 	address = "16 Long Street",
+		-- 	phone = "123456"
+		--  },
+	  
+		--  {
+		-- 	name = "Barney",
+		-- 	address = "17 Long Street",
+		-- 	phone = "123457"
+		--  } 	
+		
+	 }
+
+	 local wilma = {
+		name = "Wilma",
+		address = "16 Long Street",
+		phone = "123456"
+	 }
+
+	 table.insert(people.Professions, wilma)
+
+	for index, value in ipairs(people.Professions) do 
+		self:Print(tostring(index).." : "..value.name)
+	end
+
 end
