c3e20949062c38d30ad8dff08f179dbf8ba25422
Joe Gannon <joegannon15@gmail.com>
2019-07-29 11:50:37 -0400

    Initial Commit

---
+++ TrackSales.lua
@@ -0,0 +1,9 @@
+
+TrackSales = LibStub("AceAddon-3.0"):NewAddon("TrackSales", "AceConsole-3.0")
+
+function TrackSales:OnInitialize()
+
+	--message('HelloWorld')
+	self:Print("Hello World!")	
+
+end
